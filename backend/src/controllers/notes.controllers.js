6f7eb2e7e18cfee89f43cfc5d0af7f6f0d81136a
kaushl12 <kaushalchaudhari82@.com>
2025-12-02 16:45:32 +0530

    update createNotes and add updateNote and deleteNote endpoint

--- backend/src/controllers/notes.controllers.js
+++ backend/src/controllers/notes.controllers.js
@@ -3,6 +3,7 @@ import { asyncHandler } from "../utils/asyncHandlers.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Note } from "../models/notes.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import mongoose, { mongo } from "mongoose";
 
 const notesSchema = z.object({
   title: z.string().trim().min(1, "Title is Required"),
@@ -12,39 +13,107 @@ const notesSchema = z.object({
 export const createNotes = asyncHandler(async (req, res) => {
   const notesData = notesSchema.safeParse(req.body);
   if (!notesData.success) {
-    return res.status(400).json({
-      message: "Invalid Format",
-      error: notesData.error.issues,
-    });
+    throw new ApiError(400, "Invalid note format", notesData.error.issues);
   }
 
-  const {title,content}=notesData.data;
-  const userId=req.user?._id;
-  if(!userId){
-    throw new ApiError(401,"Unauthorized Access")
+  const { title, content } = notesData.data;
+  const userId = req.user?._id;
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized Access");
   }
-  const note=await Note.create({
-    title:title.trim(),
-    content:content,
-    user:userId
-  })
-  if(!note){
-    throw new ApiError(500,"Note could not be created")
+  const note = await Note.create({
+    title: title.trim(),
+    content: content.trim(),
+    user: userId,
+  });
+
+  if (!note) {
+    throw new ApiError(500, "Note could not be created");
   }
-  return res.status(200).json(
-    new ApiResponse(200,note,"Note Created Successfully")
-  )
+  const cleanNote = await Note.findById(note._id).select("-__v").lean();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, cleanNote, "Note Created Successfully"));
 });
 export const getAllNotes = asyncHandler(async (req, res) => {
   res.send("Hello in all");
 });
 
 export const updateNotes = asyncHandler(async (req, res) => {
-  res.send("Hello in update");
+  if (!req.user || !req.user._id) {
+    throw new ApiError(401, "Unauthorized Access");
+  }
+  const userId = req.user._id;
+  const { noteId } = req.params;
+  if (!noteId) {
+    throw new ApiError(400, "Missing Note Id in request");
+  }
+  if (!mongoose.Types.ObjectId.isValid(noteId)) {
+    throw new ApiError(400, "Invalid Note Id");
+  }
+  const updateNotesSchema = notesSchema.partial();
+
+  const updateData = updateNotesSchema.safeParse(req.body);
+
+  if (!updateData.success) {
+    throw new ApiError(
+      400,
+      "Invalid Update note format",
+      updateData.error.issues
+    );
+  }
+  const { title, content } = updateData.data;
+  const existingNote = await Note.findById({ _id: noteId, user: userId });
+  if (!existingNote) {
+    throw new ApiError(
+      404,
+      "Note not found or you are not allowed to update it"
+    );
+  }
+  const updatedNote = await Note.findByIdAndUpdate(
+    noteId,
+    {
+      title: title,
+      content: content,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+    .select("-__v")
+    .lean();
+  if (!updatedNote) {
+    throw new ApiError(500, "Note update failed");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedNote, "Note Updated Successfully"));
 });
 
 export const deleteNotes = asyncHandler(async (req, res) => {
-  res.send("Hello in dekete");
+   if (!req.user?._id) {
+    throw new ApiError(401, "Unauthorized Access");
+  }
+  const userId = req.user._id;
+  const { noteId } = req.params;
+  if (!noteId) {
+    throw new ApiError(400, "Missing Note Id in request");
+  }
+  if (!mongoose.Types.ObjectId.isValid(noteId)) {
+    throw new ApiError(400, "Invalid Note Id");
+  }
+
+  const deletedNote=await Note.deleteOne({_id:noteId, user:userId})
+ 
+   if (deletedNote.deletedCount === 0) {
+    throw new ApiError(404, "Note not found or not owned by user");
+  }
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200,{},"Note Deleted Successfully")
+  )
 });
 export const getNote = asyncHandler(async (req, res) => {
   res.send("Hello in notes");
--- backend/src/routes/notes.routes.js
+++ backend/src/routes/notes.routes.js
@@ -5,7 +5,7 @@ import { verifyJwt } from "../middleware/auth.middleware.js";
 const router=Router();
 router.route('/create').post(verifyJwt,createNotes)
 router.route('/all').get(getAllNotes)
-router.route('/update/:id').patch(updateNotes)
-router.route('/delete/:id').delete(deleteNotes)
+router.route('/update/:noteId').patch(verifyJwt,updateNotes)
+router.route('/delete/:noteId').delete(verifyJwt,deleteNotes)
 router.route('/get/:id').get(getNote)
 export default router;
